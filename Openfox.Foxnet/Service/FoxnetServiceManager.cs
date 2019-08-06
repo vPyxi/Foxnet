39f93113b29c2aaba9bcd2325c83e2baaf4f50ea
vPyxi <43400068+vPyxi@users.noreply.github.com>
2019-08-06 02:39:21 -0500

    Make FoxnetServiceManager a singleton

--- Openfox.Foxnet/Service/FoxnetServiceManager.cs
+++ Openfox.Foxnet/Service/FoxnetServiceManager.cs
@@ -2,6 +2,16 @@ namespace Openfox.Foxnet.Service
 {
     public class FoxnetServiceManager
     {
-        
+        private static readonly Lazy<FoxnetServiceManager> _instance = new Lazy<FoxnetServiceManager>(() => new FoxnetServiceManager());
+
+        public static FoxnetServiceManager Instance 
+        {
+            get => _instance.Value;
+        }
+
+        private FoxnetServiceManager()
+        {
+            
+        }
     }
 }
